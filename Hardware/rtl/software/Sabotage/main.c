ac342fb7af32f5a7558dfc711a5d83bbae12c5a2
boweiren <bren01@student.ubc.ca>
2022-03-29 16:53:08 -0700

    updated main.c

--- Hardware/rtl/software/Sabotage/main.c
+++ Hardware/rtl/software/Sabotage/main.c
@@ -1,33 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-
-#include "sys/alt_log_printf.h"
-
-#include "gps.h"
-#include "bluetooth.h"
-#include "debug.h"
-#include "touch_to_hps.h"
-
-#define GPS 0
+#include "touchscreen.h"
 
 int main(void)
 {
-
-    if (GPS) {
-    	if (gps_driver() < 0) printf("GPS driver unsuccessful\n");
-    } else {
-    	while (1) {
-    		write_sw_data();
-    		usleep(1000000);
-    	}
-    }
+	if (Init_Touch() == -1) printf("Error\n");
 
 	printf("Done\n");
 	return 0;
 }
-
-
-
-
